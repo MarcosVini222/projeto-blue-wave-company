05046693c1872ceed71c07ac834d83aa0eeb5d22
HeitorOrtega <rm5578@fiap.com.br>
2024-06-05 19:19:38 -0400

    Terminado a função login, e linkando com a página login

--- js/script.js
+++ js/script.js
@@ -15,4 +15,22 @@ function login(event){
 
     const user = users.find(u => u.username === username && u.password === password);
 
+    if(user){
+        sessionStorage.setItem("user", JSON.stringify(user));
+        const sucessMessage = document.getElementById("sucessoLogin");
+        if(sucessMessage){
+            sucessMessage.textContent = "Login realizado com sucesso!";
+            sucessMessage.classList.add("login-sucess");
+        }
+        setTimeout(() => {
+            window.location.href = "index.html";
+        }, 3000);
+    }else{
+        const errorElement = document.getElementById("erroLogin");
+        errorElement.textContent = "Usuário ou senha incorretos ou inválidos"
+        setTimeout(() => {
+            errorElement.textContent = "";
+        }, 5000);
+    }
+
 }
