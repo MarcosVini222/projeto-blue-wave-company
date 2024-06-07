03f020fcf89bba0c5998d88d041bcf33211013d5
MarcosVini222 <lourencmarcos@gmail.com>
2024-06-07 18:44:04 -0300

    Adicionando chat bot ao nosso site

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const users = [
     { username: "BlueWaveUser", password: "@12345", name: "Marcos" },
-    { username: "BlueWaveUser1", password: "@5678", name: "João" }
+    { username: "BlueWaveUser1", password: "@5678", name: "João" },
   ];
 
   function login(event) {
@@ -14,7 +14,9 @@ document.addEventListener("DOMContentLoaded", function() {
     const username = usernameInput.value.trim();
     const password = passwordInput.value.trim();
 
-    const user = users.find(u => u.username === username && u.password === password);
+    const user = users.find(
+      (u) => u.username === username && u.password === password
+    );
 
     if (user) {
       sessionStorage.setItem("user", JSON.stringify(user));
@@ -23,10 +25,10 @@ document.addEventListener("DOMContentLoaded", function() {
         successMessage.textContent = "Login realizado com sucesso!";
         successMessage.classList.add("login-success");
       }
-      
-      setTimeout(function() {
+
+      setTimeout(function () {
         window.location.href = "index.html";
-      }, 1000); 
+      }, 1000);
     } else {
       const errorElement = document.getElementById("erroLogin");
       errorElement.textContent = "Username ou senha inválidos";
@@ -49,10 +51,27 @@ document.addEventListener("DOMContentLoaded", function() {
 
   const loginForm = document.getElementById("loginForm");
   if (loginForm) {
-    loginForm.addEventListener("submit", login); 
+    loginForm.addEventListener("submit", login);
   }
 
   if (window.location.pathname.includes("index.html")) {
     checkAuth();
   }
+});
+
+window.watsonAssistantChatOptions = {
+  integrationID: "9c5ee59c-2b83-43b4-b3a5-a3da2c1d4f53",
+  region: "au-syd",
+  serviceInstanceID: "8ba7c4f8-4487-4bf1-b86a-7bd7d7009b1e",
+  onLoad: async (instance) => {
+    await instance.render();
+  },
+};
+setTimeout(function () {
+  const t = document.createElement("script");
+  t.src =
+    "https://web-chat.global.assistant.watson.appdomain.cloud/versions/" +
+    (window.watsonAssistantChatOptions.clientVersion || "latest") +
+    "/WatsonAssistantChatEntry.js";
+  document.head.appendChild(t);
 });
